511868d4105a83abb76fc3e41679320bfcf9b459
roman.tolokontsev <antbobrikov@yandex.ru>
2020-09-09 16:33:38 +0300

    first commit

---
+++ src/test/java/com/trustmecrud/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trustmecrud.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
